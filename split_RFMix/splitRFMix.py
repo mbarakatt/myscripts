6864dab0c559d8683d2cbd69239f72187b8d42ca
Maxime Barakatt Abacus <mbarakat@abacus1.(none)>
2014-06-11 16:06:11 -0400

    now read gz files also

--- split_RFMix/splitRFMix.py
+++ split_RFMix/splitRFMix.py
@@ -13,7 +13,7 @@ chr=args[3]#eg 3
 REF_START=21603
 REF_END=22078
 NB_INDIV=(REF_START-1)/2
-NB_PART=8
+NB_PART=6
 starts=[ (mult*NB_INDIV)/NB_PART*2+1 for mult in range(0,NB_PART)]
 ends=[ (mult*NB_INDIV)/NB_PART*2 for mult in range(1,NB_PART+1)]
 
@@ -21,5 +21,5 @@ ends=[ (mult*NB_INDIV)/NB_PART*2 for mult in range(1,NB_PART+1)]
 for i in range(NB_PART):
 	field_part= str(starts[i]) + "-" + str(ends[i]) + "," + str(REF_START) +"-" + str(REF_END)+ " "
 	print "Iteration", str(i), 'cut -d " " ' + field_part  + os.path.join(folder,"alleles","alleles."+name+ ".chr"+ str(chr) + ".txt" ) + " > " + os.path.join(folder,"alleles","alleles."+name+ ".chr" + str(chr) + "." + str(i)+".txt  " )
-	os.system('cut -c' + field_part  + os.path.join(folder,"alleles","alleles."+name+ ".chr"+ str(chr) + ".txt" ) + " > " + os.path.join(folder,"alleles","alleles."+name+ ".chr" + str(chr) + "." + str(i)+".txt &" ))
+	os.system('cut -c' + field_part  + os.path.join(folder,"alleles","alleles."+name+ ".chr"+ str(chr) + ".txt" ) + " > " + os.path.join(folder,"alleles","alleles."+name+ ".chr" + str(chr) + "." + str(i)+".txt " ))
 	os.system('cut -d " " -f' + field_part  + os.path.join(folder,"classes","classes."+name+ ".chr"+ str(chr) + ".txt" ) + " > " + os.path.join(folder,"classes","classes."+name+ ".chr" + str(chr) + "." + str(i)+".txt" ))
