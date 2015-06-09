d9902dd6ffc61b09401de4ab7dd82b107e6b5db6
Maxime Barakatt <mbarakatt@gmail.com>
2015-06-09 10:57:09 -0400

    initial commit

---
+++ selectsnp/selectsnp.py
@@ -0,0 +1,22 @@
+import sys
+fin=sys.stdin
+fout=sys.stdout
+args = sys.argv
+
+#usage= python selectsnp.py file_with_snps
+
+fsnp=open(args[1]) 
+
+snps=[]
+
+for line in fsnp:
+	snps.append(int(line[0:-1]))
+
+index=0
+for line in fin:
+	sp=line.split()
+	if int(sp[2]) ==  snps[index]:
+		fout.write(line)
+		index = index + 1
+		if index >= len(snps):
+			break
