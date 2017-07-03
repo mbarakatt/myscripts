c7a7629fe03cedd89f23f459062f98029f90b0ab
Maxime Barakatt <mbarakatt@gmail.com>
2017-07-03 11:38:02 -0400

    updated

--- sshfstools/mount_briaree.sh
+++ sshfstools/mount_briaree.sh
@@ -2,6 +2,6 @@ diskutil unmount force ~/briaree
 mkdir -p ~/briaree
 
 # briaree has to be a defined host in ~/.ssh/config
-sshfs -o allow_other -o follow_symlinks -o volname=briaree  briaree:/exec5/GROUP/hussinju/COMMUN/hussinju_group/lab_projects ~/briaree
+sshfs -o allow_other -o follow_symlinks -o volname=briaree  briaree:/exec5/GROUP/hussinju/COMMUN/hussinju_group/ ~/briaree
 
 
--- sshfstools/mount_graham.sh
+++ sshfstools/mount_graham.sh
@@ -1,5 +1,5 @@
 mkdir -p ~/graham
-diskutil unmount force ~/briaree
+diskutil unmount force ~/graham
 
 # briaree has to be a defined host in ~/.ssh/config
 sshfs -o allow_other -o follow_symlinks -o volname=graham  graham:/project/hussinju/shared/lab_projects ~/graham
