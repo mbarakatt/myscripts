ad3e77b2cbf4dde4d59f43da9f6575874f6a04ef
mbarakatt@gmail.com <mbarakatt@gmail.com>
2017-07-05 13:57:30 -0400

    works on linux

--- sshfstools/mount_graham.sh
+++ sshfstools/mount_graham.sh
@@ -1,7 +1,15 @@
 mkdir -p ~/graham
-diskutil unmount force ~/graham
+
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+	umount ~/graham
+elif [[ "$unamestr" == 'Darwin' ]]; then
+	diskutil unmount force ~/graham
+fi
+
 
 # briaree has to be a defined host in ~/.ssh/config
-sshfs -o allow_other -o follow_symlinks -o volname=graham  graham:/project/hussinju/shared/lab_projects ~/graham
+# sshfs -o allow_other -o follow_symlinks -o volname=graham  graham:/project/hussinju/shared/lab_projects ~/graham
+sshfs -o follow_symlinks graham:/project/hussinju/shared/lab_projects ~/graham
 
 
